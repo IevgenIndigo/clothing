c32eb9fd11931ee3d39dbf771def2a3dbc5e31a4
yevhens <yevhen.shykhaliiev@playtech.com>
2022-10-18 22:14:15 +0300

    navigation

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,6 +1,5 @@
-import SHOP_DATA from '../shop-data.js';
 import {createContext, useEffect, useState} from "react";
-import {addCollectionAndDocuments, getCategoriesAndDocuments} from "../utils/firebase/firebase.utils";
+import {getCategoriesAndDocuments} from "../utils/firebase/firebase.utils";
 
 export const CategoriesContext = createContext({
 	categoriesMap: {}
@@ -17,7 +16,6 @@ export const CategoriesProvider = ({children}) => {
 	useEffect(() => {
 		const getCategoriesMap = async () => {
 			const categoryMap = await getCategoriesAndDocuments();
-			console.log(categoryMap);
 			setCategoriesMap(categoryMap);
 		}
 
